bae4363ddcee871f0354cfaadda9ed79a1113744
WeiDashan <767708152@qq.com>
2024-02-19 16:03:23 +0800

    commit

--- src/views/login/findpassword.vue
+++ src/views/login/findpassword.vue
@@ -16,17 +16,17 @@
 			:append-to-body="true"
 		>
 			<el-form ref="pwdform"  :model="form" label-width="100px" :rules="rules">
-				<el-form-item label="验证码">
+				<el-form-item label="验证码" prop="code">
 					<el-input v-model="form.code"></el-input>
 				</el-form-item>
-				<el-form-item label="密码">
+				<el-form-item label="密码" prop="rawPassword">
 					<el-input v-model="form.rawPassword"></el-input>
 				</el-form-item>
-				<el-form-item label="重复密码">
+				<el-form-item label="重复密码" prop="rePassword">
 					<el-input v-model="form.rePassword"></el-input>
 				</el-form-item>
 				<el-form-item> 
-					<el-button type="primary" plain>确定</el-button>
+					<el-button type="primary" plain @click="submit">确定</el-button>
 				</el-form-item>
 			</el-form>
 		</el-dialog>
@@ -37,6 +37,15 @@
 export default {
   name: '',
   data() {
+    const validatePass2 = (rule, value, callback)=>{
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.form.rawPassword) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
     return {
 			form: {
 				username: '',
@@ -47,7 +56,16 @@ export default {
 			rules: {
 				username: [
 					{required: true, message: '请输入用户名/邮箱', trigger: 'blur'}
-				]
+				],
+        code: [
+          {required: true, message: '请输入验证码', trigger: 'blur'}
+        ],
+        rawPassword: [
+          {required: true, message: '请输入密码', trigger: 'blur'}
+        ],
+        rePassword: [
+          {required: true, validator: validatePass2, trigger: 'blur'}
+        ]
 			},
 			dialogTableVisible: false
     }
@@ -57,15 +75,37 @@ export default {
 			this.$emit('update:r',0)
 		},
 		getCode() {
-			// this.dialogTableVisible = true
-			this.$refs['form'].validateField(['username'],vaild => {
-				if(!vaild) {
-					this.post('/ums-user/getcode',{username: this.form.username}, () => {
+			this.$refs['form'].validateField(['username'],valid => {
+				if(!valid) {
+					this.post('/ums-user/getcode',{loginName: this.form.username}, () => {
 						this.dialogTableVisible = true
 					})
+          this.dialogTableVisible = true
 				}
 			})
-		}
+		},
+
+    submit(){
+      let checkList = []
+      this.$refs['pwdform'].validateField(['code','rawPassword','rePassword'],valid => {
+        if(!valid) {
+          checkList.push(valid)
+          if (checkList.length===3){
+
+            this.post('/ums-user/changePassword',
+                {loginName: this.form.username,
+                code: this.form.code,
+                password: this.form.rawPassword},
+                () => {
+                  this.dialogTableVisible = false
+                  this.form.code = ""
+                  this.form.rawPassword = ""
+                  this.form.rePassword = ""
+            })
+          }
+        }
+      })
+    }
   }
 }
 </script>
